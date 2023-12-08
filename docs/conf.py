a0975bf4901cdac9fe8ec2253f5b2bcfd695ac11
Matt Jensen <mattjensen625@gmail.com>
2023-12-08 13:56:16 -0700

    update

--- docs/conf.py
+++ docs/conf.py
@@ -28,5 +28,5 @@ source_suffix = ['.rst', '.md']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'groundwork'
 html_static_path = ['_static']
