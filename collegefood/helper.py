a9de0c98f8c38306f70847abb260eabbab69ff9b
matt9black@gmail.com <matt9black@gmail.com>
2023-12-11 18:32:46 -0700

    Adding my function

--- collegefood/helper.py
+++ collegefood/helper.py
@@ -70,3 +70,25 @@ def drop_columns(dataframe, columns_to_drop):
     """
     dataframe = dataframe.drop(columns=columns_to_drop, errors='ignore')
     return dataframe
+
+def cleaning_col(column,type,data):
+    """
+    Converts specified column to new data type and deleting observations that do not apply
+
+    Parameters:
+        column (string): The column that needs to be edited
+        type (data type): The data type that is desired
+        data (pd.DataFrame): The DataFrame that is needing to be changed
+
+    Returns:
+        pd.DataFrame: DataFrame with correct column type
+    """
+    my_array = []
+    for i in data[column]:
+        try:
+            temp = type(i)
+            my_array.append(temp)
+        except ValueError:
+            temp = np.nan
+            my_array.append(temp)
+    data[column] = my_array
